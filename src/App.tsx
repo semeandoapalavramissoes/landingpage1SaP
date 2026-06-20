0bfb4ccd9289e70c76f0719fb445b67670bbbdbb
semeandoapalavramissoes <semeandoapalavra.missoes@gmail.com>
2026-06-20 11:31:07 -0300

    ATUALIZAÇÃO DO LINK PROG 03

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import logo from "./assets/logo.png";
 
 function App() {
   // Links configuráveis
-  const youtubeLiveUrl = "https://www.youtube.com/embed/7dERmp4vt_I?si=S_X-SM4A9YunV01z";
+  const youtubeLiveUrl = "https://www.youtube.com/embed/m9DhoiALQqg?si=tvDVxXF74zubjIxw";
   const donationLink = "https://link.mercadopago.com.br/missemeandoapalavra";
 
   const getYoutubeId = (url: string) => {
